86094d360abd298ed1d23f0adcd51d9f17b50556
Vicky01012004 <vickydwiananto7@gmail.com>
2025-06-01 01:00:45 +0700

    make API chart & fix chart

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ChartController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,7 @@ Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 ], function ($router) {
+    Route::get('chart', [ChartController::class, 'getChartData']);
     // Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('logout');
     Route::post('/me', [AuthController::class, 'me'])->middleware('auth:api')->name('me');
